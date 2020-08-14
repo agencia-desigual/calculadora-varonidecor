c4eef13f347b738ebdaff0c6049d69b3a3e4c996
Edilson Pereira <edilson.pereira.iami@gmail.com>
2020-08-14 17:00:07 -0300

    Estrutura site

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,29 +1,504 @@
+var Dados = {
+    "quantidadeParedes" : "0",
+    "quantidadePortas" : "0",
+    "quantidadeJanelas" : "0",
+    "metragemParedes" : "0",
+    "metragemPortas" : "0",
+    "metragemJanelas" : "0"
+};
+
+$(".maskValor").mask("###0.00", {reverse: true});
 
 
+
+/**
+ * Metodo para selecionar a quantidade de paredes
+ * de maneira dinamicamente (GAMBIARRA)
+ */
 $("#quantideParede").change(function () {
 
     // Pegando a quantidade selecionada
     var quantideParede = $(this).val();
+    Dados.quantidadeParedes = $(this).val();
     var i = 1;
-    var paredes = "";
 
-    // Gerando os inputs da parede
+    $("#parede1").css("display","none");
+    $("#parede2").css("display","none");
+    $("#parede3").css("display","none");
+    $("#parede4").css("display","none");
+
+    // Mostrando os inputs da parede
     for (i = 1; i <= quantideParede; i++)
     {
-
-        paredes += "" +
-            "<div class=\"row\">\n" +
-            "   <div class=\"col-12\"><p>Parede "+i+"</p><hr></div>\n" +
-            "   <div class=\"form-group col-md-6\">\n" +
-            "       <label>Altura</label>\n" +
-            "       <input type=\"tel\" class=\"form-control\" placeholder=\"Ex. 3.00\">\n" +
-            "   </div>\n" +
-            "   <div class=\"form-group col-md-6\">\n" +
-            "       <label>Largura</label>\n" +
-            "       <input type=\"tel\" class=\"form-control\" placeholder=\"Ex. 5.00\">\n" +
-            "   </div>\n" +
-            "</div>";
+        $("#parede"+i).css("display","flex");
     }
-    $("#paredes").html(paredes);
 
 });
+
+
+
+/**
+ * Metodo para selecionar a quantidade de paredes
+ * de maneira dinamicamente (GAMBIARRA)
+ */
+$("#quantidadePortas").change(function () {
+
+    // Pegando a quantidade selecionada
+    var quantidePortas = $(this).val();
+    Dados.quantidePortas = $(this).val();
+    var i = 1;
+
+    $("#porta1").css("display","none");
+    $("#porta2").css("display","none");
+    $("#porta3").css("display","none");
+    $("#porta4").css("display","none");
+    $("#porta5").css("display","none");
+    $("#porta6").css("display","none");
+    $("#porta7").css("display","none");
+    $("#porta8").css("display","none");
+    $("#porta9").css("display","none");
+    $("#porta10").css("display","none");
+
+    // Mostrando os inputs da parede
+    for (i = 1; i <= quantidePortas; i++)
+    {
+        $("#porta"+i).css("display","flex");
+    }
+
+});
+
+
+
+/**
+ * Metodo para selecionar a quantidade de paredes
+ * de maneira dinamicamente (GAMBIARRA)
+ */
+$("#quantidadeJanelas").change(function () {
+
+    // Pegando a quantidade selecionada
+    var quantideJanelas = $(this).val();
+    Dados.quantideJanelas = $(this).val();
+    var i = 1;
+
+    $("#janela1").css("display","none");
+    $("#janela2").css("display","none");
+    $("#janela3").css("display","none");
+    $("#janela4").css("display","none");
+    $("#janela5").css("display","none");
+    $("#janela6").css("display","none");
+    $("#janela7").css("display","none");
+    $("#janela8").css("display","none");
+    $("#janela9").css("display","none");
+    $("#janela10").css("display","none");
+
+    // Mostrando os inputs quantideJanelas parede
+    for (i = 1; i <= quantideJanelas; i++)
+    {
+        $("#janela"+i).css("display","flex");
+    }
+
+});
+
+
+
+/**
+ * Metodo por pegar todos os dados do envio do
+ * formulario e validar
+ */
+$("#formQuantidadeParedes").on("submit", function () {
+
+    // NÃ£o atualiza a pÃ¡gina
+    event.preventDefault();
+
+    // Bloqueia o Form
+    $(this).addClass("bloqueiaForm");
+
+    // Recupera os dados importantes
+    var form = new FormData(this);
+    var calculo_parede_1 = 0;
+    var calculo_parede_2 = 0;
+    var calculo_parede_3 = 0;
+    var calculo_parede_4 = 0;
+    var metragemParedes = 0;
+
+
+
+    // PAREDE 1
+    if( (form.get("parede_1_altura") !== "" && form.get("parede_1_largura") !== "") ||
+        (form.get("parede_1_altura") !== undefined && form.get("parede_1_largura") !== undefined) ||
+        (form.get("parede_1_altura") !== null && form.get("parede_1_largura") !== null))
+    {
+        var parede_1_altura = form.get("parede_1_altura");
+        var parede_1_largura = form.get("parede_1_largura");
+        var calculo_parede_1 = parede_1_altura * parede_1_largura;
+    }
+
+
+
+    // PAREDE 2
+    if( (form.get("parede_2_altura") !== "" && form.get("parede_2_largura") !== "") ||
+        (form.get("parede_2_altura") !== undefined && form.get("parede_2_largura") !== undefined) ||
+        (form.get("parede_2_altura") !== null && form.get("parede_2_largura") !== null))
+    {
+        var parede_2_altura = form.get("parede_2_altura");
+        var parede_2_largura = form.get("parede_2_largura");
+        var calculo_parede_2 = parede_2_altura * parede_2_largura;
+    }
+
+
+
+    // PAREDE 3
+    if( (form.get("parede_3_altura") !== "" && form.get("parede_3_largura") !== "") ||
+        (form.get("parede_3_altura") !== undefined && form.get("parede_3_largura") !== undefined) ||
+        (form.get("parede_3_altura") !== null && form.get("parede_3_largura") !== null))
+    {
+        var parede_3_altura = form.get("parede_3_altura");
+        var parede_3_largura = form.get("parede_3_largura");
+        var calculo_parede_3 = parede_3_altura * parede_3_largura;
+    }
+
+
+
+    // PAREDE 4
+    if( (form.get("parede_4_altura") !== "" && form.get("parede_4_largura") !== "") ||
+        (form.get("parede_4_altura") !== undefined && form.get("parede_4_largura") !== undefined) ||
+        (form.get("parede_4_altura") !== null && form.get("parede_4_largura") !== null))
+    {
+        var parede_4_altura = form.get("parede_4_altura");
+        var parede_4_largura = form.get("parede_4_largura");
+        var calculo_parede_4 = parede_4_altura * parede_4_largura;
+    }
+
+
+
+    // SOMANDO OS CALCULOS
+    metragemParedes = calculo_parede_1 + calculo_parede_2 + calculo_parede_3 + calculo_parede_4;
+    Dados.metragemParedes = metragemParedes;
+
+    setTimeout(function () {
+
+        // Bloqueia o Form
+        $(this).removeClass("bloqueiaForm");
+
+        $("#paredes").css("display","none");
+        $("#portas").css("display","block");
+
+    },1000);
+
+    // Não atualiza mesmo
+    return false;
+});
+
+
+
+/**
+ * Metodo por pegar todos os dados do envio do
+ * formulario e validar
+ */
+$("#formQuantidadePortas").on("submit", function () {
+
+    // NÃ£o atualiza a pÃ¡gina
+    event.preventDefault();
+
+    // Bloqueia o Form
+    $(this).addClass("bloqueiaForm");
+
+    // Recupera os dados importantes
+    var form = new FormData(this);
+    var calculo_porta_1 = 0;
+    var calculo_porta_2 = 0;
+    var calculo_porta_3 = 0;
+    var calculo_porta_4 = 0;
+    var calculo_porta_5 = 0;
+    var calculo_porta_6 = 0;
+    var calculo_porta_7 = 0;
+    var calculo_porta_8 = 0;
+    var calculo_porta_9 = 0;
+    var calculo_porta_10 = 0;
+    var metragemPortas = 0;
+
+
+
+    // PORTA 1
+    if( (form.get("porta_1_altura") !== "" && form.get("porta_1_largura") !== "") ||
+        (form.get("porta_1_altura") !== undefined && form.get("porta_1_largura") !== undefined) ||
+        (form.get("porta_1_altura") !== null && form.get("porta_1_largura") !== null))
+    {
+        var porta_1_altura = form.get("porta_1_altura");
+        var porta_1_largura = form.get("porta_1_largura");
+        var calculo_porta_1 = porta_1_altura * porta_1_largura;
+    }
+
+    // PORTA 2
+    if( (form.get("porta_2_altura") !== "" && form.get("porta_2_largura") !== "") ||
+        (form.get("porta_2_altura") !== undefined && form.get("porta_2_largura") !== undefined) ||
+        (form.get("porta_2_altura") !== null && form.get("porta_2_largura") !== null))
+    {
+        var porta_2_altura = form.get("porta_2_altura");
+        var porta_2_largura = form.get("porta_2_largura");
+        var calculo_porta_2 = porta_2_altura * porta_2_largura;
+    }
+
+    // PORTA 3
+    if( (form.get("porta_3_altura") !== "" && form.get("porta_3_largura") !== "") ||
+        (form.get("porta_3_altura") !== undefined && form.get("porta_3_largura") !== undefined) ||
+        (form.get("porta_3_altura") !== null && form.get("porta_3_largura") !== null))
+    {
+        var porta_3_altura = form.get("porta_3_altura");
+        var porta_3_largura = form.get("porta_3_largura");
+        var calculo_porta_3 = porta_3_altura * porta_3_largura;
+    }
+
+    // PORTA 4
+    if( (form.get("porta_4_altura") !== "" && form.get("porta_4_largura") !== "") ||
+        (form.get("porta_4_altura") !== undefined && form.get("porta_4_largura") !== undefined) ||
+        (form.get("porta_4_altura") !== null && form.get("porta_4_largura") !== null))
+    {
+        var porta_4_altura = form.get("porta_4_altura");
+        var porta_4_largura = form.get("porta_4_largura");
+        var calculo_porta_4 = porta_4_altura * porta_4_largura;
+    }
+
+    // PORTA 5
+    if( (form.get("porta_5_altura") !== "" && form.get("porta_5_largura") !== "") ||
+        (form.get("porta_5_altura") !== undefined && form.get("porta_5_largura") !== undefined) ||
+        (form.get("porta_5_altura") !== null && form.get("porta_5_largura") !== null))
+    {
+        var porta_5_altura = form.get("porta_5_altura");
+        var porta_5_largura = form.get("porta_5_largura");
+        var calculo_porta_5 = porta_5_altura * porta_5_largura;
+    }
+
+    // PORTA 6
+    if( (form.get("porta_6_altura") !== "" && form.get("porta_6_largura") !== "") ||
+        (form.get("porta_6_altura") !== undefined && form.get("porta_6_largura") !== undefined) ||
+        (form.get("porta_6_altura") !== null && form.get("porta_6_largura") !== null))
+    {
+        var porta_6_altura = form.get("porta_6_altura");
+        var porta_6_largura = form.get("porta_6_largura");
+        var calculo_porta_6 = porta_6_altura * porta_6_largura;
+    }
+
+    // PORTA 7
+    if( (form.get("porta_7_altura") !== "" && form.get("porta_7_largura") !== "") ||
+        (form.get("porta_7_altura") !== undefined && form.get("porta_7_largura") !== undefined) ||
+        (form.get("porta_7_altura") !== null && form.get("porta_7_largura") !== null))
+    {
+        var porta_7_altura = form.get("porta_7_altura");
+        var porta_7_largura = form.get("porta_7_largura");
+        var calculo_porta_7 = porta_7_altura * porta_7_largura;
+    }
+
+    // PORTA 8
+    if( (form.get("porta_8_altura") !== "" && form.get("porta_8_largura") !== "") ||
+        (form.get("porta_8_altura") !== undefined && form.get("porta_8_largura") !== undefined) ||
+        (form.get("porta_8_altura") !== null && form.get("porta_8_largura") !== null))
+    {
+        var porta_8_altura = form.get("porta_8_altura");
+        var porta_8_largura = form.get("porta_8_largura");
+        var calculo_porta_8 = porta_8_altura * porta_8_largura;
+    }
+
+    // PORTA 9
+    if( (form.get("porta_9_altura") !== "" && form.get("porta_9_largura") !== "") ||
+        (form.get("porta_9_altura") !== undefined && form.get("porta_9_largura") !== undefined) ||
+        (form.get("porta_9_altura") !== null && form.get("porta_9_largura") !== null))
+    {
+        var porta_9_altura = form.get("porta_9_altura");
+        var porta_9_largura = form.get("porta_9_largura");
+        var calculo_porta_9 = porta_9_altura * porta_9_largura;
+    }
+
+    // PORTA 10
+    if( (form.get("porta_10_altura") !== "" && form.get("porta_10_largura") !== "") ||
+        (form.get("porta_10_altura") !== undefined && form.get("porta_10_largura") !== undefined) ||
+        (form.get("porta_10_altura") !== null && form.get("porta_10_largura") !== null))
+    {
+        var porta_10_altura = form.get("porta_10_altura");
+        var porta_10_largura = form.get("porta_10_largura");
+        var calculo_porta_10 = porta_10_altura * porta_10_largura;
+    }
+
+    // SOMANDO OS CALCULOS
+    metragemPortas = calculo_porta_1 + calculo_porta_2 + calculo_porta_3 + calculo_porta_4 + calculo_porta_5 + calculo_porta_6 + calculo_porta_7 + calculo_porta_8 + calculo_porta_9 + calculo_porta_10;
+    Dados.metragemPortas = metragemPortas;
+
+
+    setTimeout(function () {
+
+        // Bloqueia o Form
+        $(this).removeClass("bloqueiaForm");
+
+        $("#paredes").css("display","none");
+        $("#portas").css("display","none");
+        $("#janelas").css("display","block");
+
+    },1000);
+
+    // Não atualiza mesmo
+    return false;
+});
+
+
+
+/**
+ * Metodo por pegar todos os dados do envio do
+ * formulario e validar
+ */
+$("#formQuantidadeJanelas").on("submit", function () {
+
+    // NÃ£o atualiza a pÃ¡gina
+    event.preventDefault();
+
+    // Bloqueia o Form
+    $(this).addClass("bloqueiaForm");
+
+    // Recupera os dados importantes
+    var form = new FormData(this);
+    var calculo_janela_1 = 0;
+    var calculo_janela_2 = 0;
+    var calculo_janela_3 = 0;
+    var calculo_janela_4 = 0;
+    var calculo_janela_5 = 0;
+    var calculo_janela_6 = 0;
+    var calculo_janela_7 = 0;
+    var calculo_janela_8 = 0;
+    var calculo_janela_9 = 0;
+    var calculo_janela_10 = 0;
+    var metragemJanelas = 0;
+
+
+
+    // JANELA 1
+    if( (form.get("janela_1_altura") !== "" && form.get("janela_1_largura") !== "") ||
+        (form.get("janela_1_altura") !== undefined && form.get("janela_1_largura") !== undefined) ||
+        (form.get("janela_1_altura") !== null && form.get("janela_1_largura") !== null))
+    {
+        var janela_1_altura = form.get("janela_1_altura");
+        var janela_1_largura = form.get("janela_1_largura");
+        var calculo_janela_1 = janela_1_altura * janela_1_largura;
+    }
+
+    // JANELA 2
+    if( (form.get("janela_2_altura") !== "" && form.get("janela_2_largura") !== "") ||
+        (form.get("janela_2_altura") !== undefined && form.get("janela_2_largura") !== undefined) ||
+        (form.get("janela_2_altura") !== null && form.get("janela_2_largura") !== null))
+    {
+        var janela_2_altura = form.get("janela_2_altura");
+        var janela_2_largura = form.get("janela_2_largura");
+        var calculo_janela_2 = janela_2_altura * janela_2_largura;
+    }
+
+    // JANELA 3
+    if( (form.get("janela_3_altura") !== "" && form.get("janela_3_largura") !== "") ||
+        (form.get("janela_3_altura") !== undefined && form.get("janela_3_largura") !== undefined) ||
+        (form.get("janela_3_altura") !== null && form.get("janela_3_largura") !== null))
+    {
+        var janela_3_altura = form.get("janela_3_altura");
+        var janela_3_largura = form.get("janela_3_largura");
+        var calculo_janela_3 = janela_3_altura * janela_3_largura;
+    }
+
+    // JANELA 4
+    if( (form.get("janela_4_altura") !== "" && form.get("janela_4_largura") !== "") ||
+        (form.get("janela_4_altura") !== undefined && form.get("janela_4_largura") !== undefined) ||
+        (form.get("janela_4_altura") !== null && form.get("janela_4_largura") !== null))
+    {
+        var janela_4_altura = form.get("janela_4_altura");
+        var janela_4_largura = form.get("janela_4_largura");
+        var calculo_janela_4 = janela_4_altura * janela_4_largura;
+    }
+
+    // JANELA 5
+    if( (form.get("janela_5_altura") !== "" && form.get("janela_5_largura") !== "") ||
+        (form.get("janela_5_altura") !== undefined && form.get("janela_5_largura") !== undefined) ||
+        (form.get("janela_5_altura") !== null && form.get("janela_5_largura") !== null))
+    {
+        var janela_5_altura = form.get("janela_5_altura");
+        var janela_5_largura = form.get("janela_5_largura");
+        var calculo_janela_5 = janela_5_altura * janela_5_largura;
+    }
+
+    // JANELA 6
+    if( (form.get("janela_6_altura") !== "" && form.get("janela_6_largura") !== "") ||
+        (form.get("janela_6_altura") !== undefined && form.get("janela_6_largura") !== undefined) ||
+        (form.get("janela_6_altura") !== null && form.get("janela_6_largura") !== null))
+    {
+        var janela_6_altura = form.get("janela_6_altura");
+        var janela_6_largura = form.get("janela_6_largura");
+        var calculo_janela_6 = janela_6_altura * janela_6_largura;
+    }
+
+    // JANELA 7
+    if( (form.get("janela_7_altura") !== "" && form.get("janela_7_largura") !== "") ||
+        (form.get("janela_7_altura") !== undefined && form.get("janela_7_largura") !== undefined) ||
+        (form.get("janela_7_altura") !== null && form.get("janela_7_largura") !== null))
+    {
+        var janela_7_altura = form.get("janela_7_altura");
+        var janela_7_largura = form.get("janela_7_largura");
+        var calculo_janela_7 = janela_7_altura * janela_7_largura;
+    }
+
+    // JANELA 8
+    if( (form.get("janela_8_altura") !== "" && form.get("janela_8_largura") !== "") ||
+        (form.get("janela_8_altura") !== undefined && form.get("janela_8_largura") !== undefined) ||
+        (form.get("janela_8_altura") !== null && form.get("janela_8_largura") !== null))
+    {
+        var janela_8_altura = form.get("janela_8_altura");
+        var janela_8_largura = form.get("janela_8_largura");
+        var calculo_janela_8 = janela_8_altura * janela_8_largura;
+    }
+
+    // JANELA 9
+    if( (form.get("janela_9_altura") !== "" && form.get("janela_9_largura") !== "") ||
+        (form.get("janela_9_altura") !== undefined && form.get("janela_9_largura") !== undefined) ||
+        (form.get("janela_9_altura") !== null && form.get("janela_9_largura") !== null))
+    {
+        var janela_9_altura = form.get("janela_9_altura");
+        var janela_9_largura = form.get("janela_9_largura");
+        var calculo_janela_9 = janela_9_altura * janela_9_largura;
+    }
+
+    // JANELA 10
+    if( (form.get("janela_10_altura") !== "" && form.get("janela_10_largura") !== "") ||
+        (form.get("janela_10_altura") !== undefined && form.get("janela_10_largura") !== undefined) ||
+        (form.get("janela_10_altura") !== null && form.get("janela_10_largura") !== null))
+    {
+        var janela_10_altura = form.get("janela_10_altura");
+        var janela_10_largura = form.get("janela_10_largura");
+        var calculo_janela_10 = janela_10_altura * janela_10_largura;
+    }
+
+    // SOMANDO OS CALCULOS
+    metragemJanelas = calculo_janela_1 + calculo_janela_2 + calculo_janela_3 + calculo_janela_4 + calculo_janela_5 + calculo_janela_6 + calculo_janela_7 + calculo_janela_8 + calculo_janela_9 + calculo_janela_10;
+    Dados.metragemJanelas = metragemJanelas;
+
+    var resultado = CalculaMetragem();
+
+    alert(resultado);
+
+    setTimeout(function () {
+
+        // Bloqueia o Form
+        $(this).removeClass("bloqueiaForm");
+
+        $("#paredes").css("display","none");
+        $("#portas").css("display","none");
+        $("#janelas").css("display","none");
+
+    },1000);
+
+    // Não atualiza mesmo
+    return false;
+});
+
+
+
+function CalculaMetragem() {
+
+    var resultado = 0;
+
+    resultado = Dados.metragemParedes - (Dados.metragemPortas + Dados.metragemJanelas);
+
+    return resultado;
+}
